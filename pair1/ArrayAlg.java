b9f76d3a883275d01221c637568d44e50fd39d2e
Nathaniel <linlilulll@gmail.com>
2015-08-15 12:06:02 +0800

    Using for each

--- pair1/ArrayAlg.java
+++ pair1/ArrayAlg.java
@@ -11,9 +11,9 @@ public class ArrayAlg {
         }
         String min = a[0];
         String max = a[0];
-        for (int i = 0; i < a.length; i++) {
-            if (min.compareTo(a[i])>0) min = a[i];
-            if (max.compareTo(a[i])<0) max = a[i];
+        for (String anA : a) {
+            if (min.compareTo(anA) > 0) min = anA;
+            if (max.compareTo(anA) < 0) max = anA;
         }
         return new Pair<>(min,max);
     }
